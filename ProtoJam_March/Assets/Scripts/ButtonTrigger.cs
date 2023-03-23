baac8abfbe97fd7af8d00314237fa5be7f135502
r01ex <102461975+r01ex@users.noreply.github.com>
2023-03-23 14:13:39 +0900

    btn ability interaction
    버튼-플레이어 능력 상호작용 추가

--- ProtoJam_March/Assets/Scripts/ButtonTrigger.cs
+++ ProtoJam_March/Assets/Scripts/ButtonTrigger.cs
@@ -9,12 +9,38 @@ public class ButtonTrigger : MonoBehaviour
     public TypeOfButton ButtonType;     //버튼 타입 지정
     public UnityEvent OnButtonPressed;  //버튼이 눌렸을 때 실행할 함수(잠긴문 열기 라든지...)
     public UnityEvent OnButtonRelease;  //버튼이 풀렸을 때 실행할 함수(홀드 버튼용)
+    bool isAbilityOn = false;
 
     [SerializeField] private List<Transform> objectWhoPressedButton; //버튼을 누르고 있는 객체 리스트
     private bool isButtonPressed = false;   //(내부용)버튼 눌렀는가?
 
-    
-    
+    private void Start()
+    {
+        playerScript.Instance.onAbilityActive.AddListener(onPlayerAbilityOn);
+        playerScript.Instance.onAbilityDeactive.AddListener(onPlayerAbilityOff);
+    }
+
+    void onPlayerAbilityOn()
+    {
+        isAbilityOn = true;
+    }
+    void onPlayerAbilityOff()
+    {
+        isAbilityOn = false;
+        if(objectWhoPressedButton.Count>0 && isButtonPressed==false)
+        {
+            isButtonPressed = true;
+            OnButtonPressed.Invoke();
+            //버튼 눌렸을 때 버튼 애니메이션
+        }
+        else if(ButtonType == TypeOfButton.Hold && isButtonPressed==true)
+        {
+            isButtonPressed = false;
+            OnButtonRelease.Invoke();
+            //버튼 풀렸을 때 버튼 애니메이션
+        }
+    }
+
     //이하 Trigger Enter/Exit 은 버튼 누른 오브젝트 감지해서 리스트에 추가
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -22,7 +48,7 @@ public class ButtonTrigger : MonoBehaviour
         {
             objectWhoPressedButton.Add(col.transform);
 
-            if (!isButtonPressed)
+            if (!isButtonPressed && isAbilityOn == false)
             {
                 isButtonPressed = true;
                 OnButtonPressed.Invoke();
@@ -37,7 +63,7 @@ public class ButtonTrigger : MonoBehaviour
         {
             objectWhoPressedButton.Remove(other.transform);
 
-            if (ButtonType == TypeOfButton.Hold && objectWhoPressedButton.Count <= 0)
+            if (ButtonType == TypeOfButton.Hold && objectWhoPressedButton.Count <= 0 && isAbilityOn == false)
             {
                 isButtonPressed = false;
                 OnButtonRelease.Invoke();
