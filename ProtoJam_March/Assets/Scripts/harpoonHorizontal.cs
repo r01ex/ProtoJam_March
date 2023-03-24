094dc993135bbf09c5c151ce2e7e3d9e45d3795b
r01ex <102461975+r01ex@users.noreply.github.com>
2023-03-24 15:25:41 +0900

    harpoon_horizontal
    가로 갈고리 구현

---
+++ ProtoJam_March/Assets/Scripts/harpoonHorizontal.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class harpoonHorizontal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] harpoontype Harpoontype;
+    [SerializeField] GameObject square;
+    [SerializeField] GameObject triangle;
+    enum harpoontype
+    {
+        triangle = 1,
+        square = 2
+    }
+    private void Start()
+    {
+        playerScript.Instance.onAbilityActive.AddListener(onPlayerAbilityOn);
+        playerScript.Instance.onAbilityDeactive.AddListener(onPlayerAbilityOff);
+        if(Harpoontype==harpoontype.triangle)
+        {
+            square.SetActive(false);
+        }
+        else
+        {
+            triangle.SetActive(false);
+        }
+    }
+
+    void onPlayerAbilityOn()
+    {
+        this.gameObject.GetComponent<Animator>().speed = 0;
+    }
+    void onPlayerAbilityOff()
+    {
+        this.gameObject.GetComponent<Animator>().speed = 1;
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ProtoJam_March/Assets/Scripts/harpoonTip.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class harpoonTip : MonoBehaviour
+{
+    [SerializeField] harpoontype Harpoontype;
+    bool isAbilityOn = false;
+    [SerializeField] float xmagnitude;
+    [SerializeField] float ymagnitude;
+    [SerializeField] float stunDuration;
+    enum harpoontype
+    {
+        triangle = 1,
+        square = 2
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && playerScript.Instance.spikehitRecent == false)
+        {
+            if (Harpoontype == harpoontype.triangle)
+            {
+                //³Ë¹é
+                playerScript.Instance.spikehitRecent = true;
+                collision.rigidbody.velocity = new Vector2(-Mathf.Sign(collision.relativeVelocity.x) * xmagnitude, ymagnitude);
+                Invoke("resetPlayer", stunDuration);
+
+            }
+            else
+            {
+                if (isAbilityOn)
+                {
+                    //nothing
+                }
+                else
+                {
+                    //³Ë¹é
+                    playerScript.Instance.spikehitRecent = true;
+                    collision.rigidbody.velocity = new Vector2(-Mathf.Sign(collision.relativeVelocity.x) * xmagnitude, ymagnitude);
+                    Invoke("resetPlayer", stunDuration);
+                }
+            }
+        }
+    }
+    void resetPlayer()
+    {
+        playerScript.Instance.spikehitRecent = false;
+    }
+    private void Start()
+    {
+        playerScript.Instance.onAbilityActive.AddListener(onPlayerAbilityOn);
+        playerScript.Instance.onAbilityDeactive.AddListener(onPlayerAbilityOff);
+    }
+
+    void onPlayerAbilityOn()
+    {
+        isAbilityOn = true;
+    }
+    void onPlayerAbilityOff()
+    {
+        isAbilityOn = false;
+    }
+}
