a73135db067cfde3886c0c3db745410d5be1699b
r01ex <102461975+r01ex@users.noreply.github.com>
2023-03-26 00:14:39 +0900

    scroll added
    능력바 테두리 추가
    스크롤 추가 (sample scene)
    스크롤 스크립트에서 ontriggerenter시 피격

---
+++ ProtoJam_March/Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+    [SerializeField] Animator scrollmove;
+    [SerializeField] Animator scrolltip;
+    private void Start()
+    {
+        playerScript.Instance.onAbilityActive.AddListener(onPlayerAbilityOn);
+        playerScript.Instance.onAbilityDeactive.AddListener(onPlayerAbilityOff);
+    }
+
+    void onPlayerAbilityOn()
+    {
+        scrollmove.speed = 0;
+        scrolltip.speed = 0;
+    }
+    void onPlayerAbilityOff()
+    {
+        scrollmove.speed = 1;
+        scrolltip.speed = 1;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            //ÇÇ°Ý
+        }
+    }
+}
